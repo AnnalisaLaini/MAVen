81b098d91ef2660ce4f87632a83507d73ce610a4
martina-boni <93976448+martina-boni@users.noreply.github.com>
2021-11-18 11:01:24 +0100

    Update holidays.py

--- holidays.py
+++ holidays.py
@@ -4,3 +4,11 @@ import requests
 import json
 
 HOLIDAYS_URL = 'https://holidayapi.com/v1/holidays?'
+
+import holidayapi
+key = '17d40907-3898-4e39-8c10-a6a9bbeb0bd1'
+hapi = holidayapi.v1(key)
+holidays = hapi.holidays({
+  'country': 'IT',
+  'year': '2021',
+})
