04f00b317c5939fe8cecca34a5e797a66fa610ab
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-12 10:04:51 -0500

    first updates to test_italianceleb.py

--- mavenpackage/testpackage/test_italianceleb.py
+++ mavenpackage/testpackage/test_italianceleb.py
@@ -1,8 +1,7 @@
 import unittest
 import sys
 import os
-from mavenpackage import italianceleb.py
-from italianceleb.py import italianceleb
+from mavenpackage import italianceleb
 
 
 '''
@@ -17,19 +16,19 @@ class TestInput(unittest.TestCase):
     # smoke test: valid inputs
     def test_italianholidays(self):
         # you should select some valid inputs, for which the output is known
-        self.assertEqual(italianceleb("All Saints' Day"), "Yup, {} is an Italian holiday!".format(someholi))
+        self.assertEqual(italianceleb.italianceleb("All Saints' Day"), "Yup, {} is an Italian holiday!".format(someholi))
 
     # invalid inputs
     def test_nonitalian(self):
         # you should input wrong data
-        self.assertEqual(italianceleb(“Thanksgiving Day"), None)
+        self.assertEqual(italianceleb.italianceleb("Thanksgiving Day"), None)
 
         # Note: the following test passing an empty list will fail!
         # self.assertEqual(italianceleb([]), None)
 
     # corner case: empty string
     def test_emptyentry(self):
-        self.assertEqual(italianceleb(""), None)
+        self.assertEqual(italianceleb.italianceleb(""), None)
 
 # code that runs the document if called
 if __name__ == '__main__':
