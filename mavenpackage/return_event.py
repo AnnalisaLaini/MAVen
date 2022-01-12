93a2140a01ec8a527cd59bb9fe381d48941048f1
martina-boni <martina.boni@student.h-farm.com>
2022-01-12 09:41:01 +0100

    upload return_event function

--- mavenpackage/return_event.py
+++ mavenpackage/return_event.py
@@ -1,1 +1,8 @@
 
+def return_event(date):
+
+    if date in dtvnt:
+        print('{} is {}.'.format(date, dtvnt[date]))
+
+    else:
+        print('Sadly, we don\'t know what is celebrated on {}\.'.format(date))
