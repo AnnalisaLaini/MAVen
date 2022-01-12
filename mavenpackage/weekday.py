a7abd96d8eef52b902afe57efd6801e7813432d7
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-12 04:37:10 -0500

    modified weekday.py

--- mavenpackage/weekday.py
+++ mavenpackage/weekday.py
@@ -1,1 +1,15 @@
+import datetime
+
+def weekday(date_str):
+    #we define a list with the days of the week
+     weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    #we transform the string to a datetime object 
+    date_obj = datetime.datetime.strptime(date_str, '20%y-%m-%d').date()
+     # weekday() returns the day of the week as an integer number, where Monday is 0 and Sunday is 6 
+     date_obj_day = date_obj.weekday()
+     #we associate the given integer with the name taken from the weekDays list we had created       previously
+     day_of_the_week = weekDays[date_obj_day]
+    #we print the outcome
+ print("The input date is on a {}".format(day_of_the_week))
+
 
