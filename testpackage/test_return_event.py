21659b7d0477cf52aa184a7bae5d17cc06708978
martina-boni <martina.boni@student.h-farm.com>
2022-01-12 11:12:50 +0100

    testing return_event

--- testpackage/test_return_event.py
+++ testpackage/test_return_event.py
@@ -1,1 +1,38 @@
+import unittest
+import sys
+import os
+from mavenpackage import return_event.py
+from return_event.py import return_event
+
+
+'''
+The following tests are applied to the return_event function.
+First, valid entries are tested: the function receives expected inputs.
+Next, the function is given invalid entries and its reaction is checked.
+Then, the performance when no inputs are provided is evaluated.
+'''
+
+class TestInput(unittest.TestCase):
+
+    # smoke test: valid inputs
+    def test_correct_values(self):
+        # you should select some valid inputs, for which the output is known
+        self.assertEqual(return_event("2023-12-26"), 'St. Stephen's Day')
+
+    # invalid inputs
+    def test_wrong_values(self):
+        # you should input wrong data
+        self.assertEqual(return_event("St. Stephen's Day"), None)
+
+        # Note: the following test passing an empty list will fail!
+        # self.assertEqual(return_event([]), None)
+
+    # corner case: empty string
+    def test_empty_string(self):
+        self.assertEqual(return_event(""), None)
+
+# code that runs the document if called
+if __name__ == '__main__':
+    unittest.main()
+
 
