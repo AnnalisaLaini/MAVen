79417416a26c59caa4e473794f3719a7544e494d
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-12 03:47:32 -0500

    added welcome function

--- main.py
+++ main.py
@@ -15,4 +15,16 @@ holidays = hapi.holidays({
 
 '''
 Implementation of methods to deal with a calendar that lists holidays
-''
+'''
+
+def welcome():
+    #we define the message and we associate the list of the events known by the software. We list those through the use of a dictionary.
+    print("Welcome to 'Holidays Project\'. We know the dates of the following events:")
+    L = []
+    for k,v in dtvnt.items():
+        if v not in L:
+            L.append(v)
+    return L
+
+welcome()
+
