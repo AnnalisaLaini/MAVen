380d5f616f0e0c3ee88700cefa4ccaccdbd8ba53
martina-boni <martina.boni@student.h-farm.com>
2022-01-12 11:20:52 +0100

    changed loc

--- testpackage/test_italianceleb.py
+++ testpackage/test_italianceleb.py
@@ -1,1 +1,37 @@
+import unittest
+import sys
+import os
+from mavenpackage import italianceleb.py
+from italianceleb.py import italianceleb
+
+
+'''
+The following tests are applied to the italianceleb function.
+First, valid entries are tested: the function receives expected inputs.
+Next, the function is given invalid entries and its reaction is checked.
+Then, the performance when no inputs are provided is evaluated.
+'''
+
+class TestInput(unittest.TestCase):
+
+    # smoke test: valid inputs
+    def test_italianholidays(self):
+        # you should select some valid inputs, for which the output is known
+        self.assertEqual(italianceleb("All Saints' Day"), "Yup, {} is an Italian holiday!".format(someholi))
+
+    # invalid inputs
+    def test_nonitalian(self):
+        # you should input wrong data
+        self.assertEqual(italianceleb(“Thanksgiving Day"), None)
+
+        # Note: the following test passing an empty list will fail!
+        # self.assertEqual(italianceleb([]), None)
+
+    # corner case: empty string
+    def test_emptyentry(self):
+        self.assertEqual(italianceleb(""), None)
+
+# code that runs the document if called
+if __name__ == '__main__':
+    unittest.main()
 
