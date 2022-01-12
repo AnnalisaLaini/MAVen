c102590de836b39efda4665cc9e6e695b6df72c5
martina-boni <martina.boni@student.h-farm.com>
2022-01-12 10:03:45 +0100

    italian celebrations function

--- mavenpackage/italianceleb.py
+++ mavenpackage/italianceleb.py
@@ -1,1 +1,6 @@
+def italianceleb(someholi):
+    if someholi in dtvnt.values():
+        print ("Yup, {} is an Italian holiday!".format(someholi))
+    else: 
+        print ("Nope, Italians do not celebrate {}.".format(someholi))
 
