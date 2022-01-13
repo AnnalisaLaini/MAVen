bd3aef932bb2b4562038a0a798f53145204325ae
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-13 10:30:17 -0500

    Final PEP8 check

--- mavenpackage/leapyear.py
+++ mavenpackage/leapyear.py
@@ -1,20 +1,32 @@
 '''
-The function leapyear, as the name suggests, is used to realize whether a year is a leapyear or not. Given the input, a year (hence an integer), the function returns "It’s a leap year" or "It’s not a leap year".
-NOTE: The input year doesn't need to be selected among the years present in the dtvnt's list; it can be any year.
-REMEMBER: A leap year is a calendar year that contains an additional day added to keep the calendar year synchronized with the astronomical year. To be a leap year, the year number must be divisible by four – except for end-of-century years, which must be divisible by 400.
+The function leapyear, as the name suggests, is used to realize
+whether a year is a leapyear or not. Given the input, a year
+(hence an integer), the function returns "It’s a leap year" or
+"It’s not a leap year".
+
+NOTE: The input year doesn't need to be selected among the years
+present in the dtvnt's list; it can be any year.
+
+REMEMBER: A leap year is a calendar year that contains an
+additional day added to keep the calendar year synchronized with
+the astronomical year. To be a leap year, the year number must be
+divisible by four – except for end-of-century years, which must be
+divisible by 400.
 '''
+
+
 def leapyear(year):
-    #year divided by 4 is a leapyear
+    # year divided by 4 is a leapyear
     if (year % 4) == 0:
         # year divided by 100 means it is a century year
         if (year % 100) == 0:
-            #century year divided by 400 is a leap year
+            # century year divided by 400 is a leap year
             if (year % 400) == 0:
                 return True
             else:
                 return False
         else:
-             return True
+            return True
     else:
         return False
 
