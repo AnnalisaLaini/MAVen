e553373fff9ad5075038345853fd469d6003b2bd
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-13 10:16:33 -0500

    Final PEP8 check

--- mavenpackage/weekday.py
+++ mavenpackage/weekday.py
@@ -1,26 +1,26 @@
-''' 
-the weekday function returns the day’s name, given the date inputted by the user
-
-''' 
+'''
+the weekday function returns the day’s name,
+given the date inputted by the user.
+'''
 
 import datetime
 
+
 def weekday(date_str):
-    #we define a list with the days of the week
-    weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-    #we transform the string to a datetime object 
-    try:  
+    # we define a list with the days of the week
+    weekDays = ("Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday")
+    # we transform the string to a datetime object
+    try:
         date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
     except:
         print("Incorrect data format, should be YYYY-MM-DD")
         return
-    
-    # weekday() returns the day of the week as an integer number, where Monday is 0 and Sunday is 6 
+    # weekday() returns the day of the week as an integer number,
+    # where Monday is 0 and Sunday is 6
     date_obj_day = date_obj.weekday()
-    #we associate the given integer with the name taken from the weekDays list we had created       previously
+    # we associate the given integer with the name taken
+    # from the weekDays list we had created previously
     day_of_the_week = weekDays[date_obj_day]
-    #we print the outcome
-    
+    # we print the outcome
     print("The requested date is on a {}".format(day_of_the_week))
-
-
