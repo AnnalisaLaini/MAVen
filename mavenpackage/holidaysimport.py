31223912e09c0ab4addda08719ef9d97bdfcc2da
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-01-13 10:54:06 -0500

    Final PEP8 check

--- mavenpackage/holidaysimport.py
+++ mavenpackage/holidaysimport.py
@@ -1,10 +1,8 @@
 '''
-THIS MODULE IS MEANT TO IMPORT HOLIDAYS DATA FROM CALENDARIFIC API AND TURN IT INTO CSV FORMAT.
+THIS MODULE IS MEANT TO IMPORT HOLIDAYS DATA FROM
+CALENDARIFIC API AND TURN IT INTO CSV FORMAT.
 '''
 
-'''
-importing packages needed to upload data
-'''
 
 import json
 import requests
@@ -12,22 +10,20 @@ from dateutil.parser import parse
 import pandas as pd
 import csv
 
-'''
-uploading data from API source using the API key; csv format is used to handle the dataset.
-'''
 
 API_KEY = '6981d02afb34fb0d7f67b5ad93491ab8283a7773'
+
 
 def CalHolidays(parameters):
     url = 'https://calendarific.com/api/v2/holidays?'
 
     parameters['api_key'] = API_KEY
 
-    response = requests.get(url, params=parameters);
+    response = requests.get(url, params=parameters)
     data = json.loads(response.text)
 
     if response.status_code != 200:
-        if data.has_key('error') is False:
+        if data.in('error') is False:
             data['error'] = 'Unknown error.'
 
     return data
@@ -37,11 +33,11 @@ holiday_list = []
 
 if __name__ == "__main__":
     country_list = [
-        {'country':'IT', 'year':2019},
-        {'country':'IT', 'year':2020},
-        {'country':'IT', 'year':2021},
-        {'country':'IT', 'year':2022},
-        {'country':'IT', 'year':2023},
+        {'country': 'IT', 'year': 2019},
+        {'country': 'IT', 'year': 2020},
+        {'country': 'IT', 'year': 2021},
+        {'country': 'IT', 'year': 2022},
+        {'country': 'IT', 'year': 2023},
     ]
     for country in country_list:
         parameters = {
@@ -67,7 +63,9 @@ if __name__ == "__main__":
             pass
 
     holidays_df = pd.DataFrame(holiday_list)
-    holidays_df.to_csv('holiday.csv', sep=',', encoding='utf-8', index=False, quoting=csv.QUOTE_ALL, quotechar='"')
+    holidays_df.to_csv('holiday.csv', sep=',', encoding='utf-8',
+                       index=False, quoting=csv.QUOTE_ALL,
+                       quotechar='"')
     # print(holidays_df)
 
 
