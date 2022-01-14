09ede8470bc3a31f72981abd847535556c44f7f9
Martina Turus <martina210300@gmail.com>
2022-01-14 10:15:20 +0100

    Final PEP8 check

--- mavenpackage/testpackage/test_weekday.py
+++ mavenpackage/testpackage/test_weekday.py
@@ -4,35 +4,34 @@ import os
 from mavenpackage import weekday
 
 
-
 '''
 The following tests are applied to the weekday function.
 The standard testing order is followed.
 '''
 
+
 class TestInput(unittest.TestCase):
 
-    # smoke test: valid inputs
+    # Smoke test: valid inputs
 
     def test_correct_values(self):
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(weekday.weekday('2022-1-10'), '0')       
-    
-    # invalid inputs
-    def test_wrong_values(self):
-        # you should input wrong data
-        self.assertEqual(weekday.weekday("Wednesday"), None)  
-        self.assertEqual(weekday.weekday("seven"), None) 
+        # You should select some valid inputs, for which the output is known
+        self.assertEqual(weekday.weekday("2022-1-10"), "0")
 
-        #NOTE: the folowing test passing an empty list will fail!
-        #self.assertEqual (weekday.weekday(), None)  
-       
-    # corner case: empty string
+    # Invalid inputs
+    def test_wrong_values(self):
+        # You should input wrong data
+        self.assertEqual(weekday.weekday("Wednesday"), None)
+        self.assertEqual(weekday.weekday("seven"), None)
+
+        # NOTE: the folowing test passing an empty list will fail!
+        # self.assertEqual (weekday.weekday(), None)
+
+    # Corner case: empty string
     def test_empty_string(self):
         with self.assertRaises(TypeError):
             weekday()
-      
-#code that runs the document if called
-if __name__ == '__main__':
-    unittest.main()
 
+# Code that runs the document if called
+if __name__ == "__main__":
+    unittest.main()
