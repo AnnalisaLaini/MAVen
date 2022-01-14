e25bb8062b718f0bc65df0b45554130fb93e5898
Martina Turus <martina210300@gmail.com>
2022-01-14 10:05:53 +0100

    Final PEP8 check

--- mavenpackage/testpackage/test_countdown.py
+++ mavenpackage/testpackage/test_countdown.py
@@ -10,19 +10,19 @@ The following tests are applied to the countdown function.
 The standard testing order is followed.
 '''
 
+
 class TestInput(unittest.TestCase):
+    # Invalid inputs
+    def test_wrong_values(self):
+        # You should input wrong data
+        self.assertEqual(countdown.countdown("2022-01-32"), None)
+        self.assertEqual(countdown.countdown("hello"), None)
+        self.assertEqual(countdown.countdown("67"), None)
 
-   # invalid inputs
-   def test_wrong_values(self):
-       # you should input wrong data
-       self.assertEqual(countdown.countdown("2022-01-32"), None)
-       self.assertEqual(countdown.countdown("hello"), None)
-       self.assertEqual(countdown.countdown("67"), None)
+        # Corner case: empty string
+        def test_empty_string(self):
+            self.assertEqual(countdown.countdown(""), None)
 
-   # corner case: empty string
-   def test_empty_string(self):
-       self.assertEqual(countdown.countdown(""), None)
-
-# code that runs the document if called
-if __name__ == '__main__':
+# Code that runs the document if called
+if __name__ == "__main__":
     unittest.main()
