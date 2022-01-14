90ea42b2e4bc129e0ff1a7f9323c31925f0551bd
Martina Turus <martina210300@gmail.com>
2022-01-14 10:11:55 +0100

    Final PEP8 check

--- mavenpackage/testpackage/test_leapyear.py
+++ mavenpackage/testpackage/test_leapyear.py
@@ -3,27 +3,31 @@ import sys
 import os
 from mavenpackage import leapyear
 
-class TestInput(unittest.TestCase):
-    
-    #smoke test: valid inputs
-    def test_leapyear(self):
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(leapyear.leapyear("2000"), "'{0} is a leap year'.format(year)")
-    
-     #wrong inputs 
-    def test_not_leapyear1(self):
-         # you should input wrong data
-        self.assertEqual(leapyear("hello"), None)         
 
-	#wrong inputs 
-    def test_not_leapyear2(self):
-         # you should input wrong data
+'''
+The following tests are applied to the weekday function.
+The standard testing order is followed.
+'''
+
+
+class TestInput(unittest.TestCase):
+
+    # Smoke test: valid inputs
+    def test_leapyear(self):
+        # You should select some valid inputs, for which the output is known
+        self.assertEqual(leapyear.leapyear("2000"), "'{0} is a leap \
+        year'.format(year)")
+
+    # Wrong inputs
+    def test_not_leapyear1(self):
+        # You should input wrong data
+        self.assertEqual(leapyear("hello"), None)
         self.assertEqual(leapyear.leapyear("twothousand"), None)
 
-    # corner case: empty string
+    # Corner case: empty string
     def test_emptyentry(self):
         self.assertEqual(leapyear.leapyear(""), None)
 
-  #code that runs the document if called
-if __name__ == '__main__':
-	unittest.main()
+# Code that runs the document if called
+if __name__ == "__main__":
+    unittest.main()
