ca0396317405b06381b66773879b9528e848231a
Martina Turus <martina210300@gmail.com>
2022-01-14 10:13:44 +0100

    Final PEP8 check

--- mavenpackage/testpackage/test_return_event.py
+++ mavenpackage/testpackage/test_return_event.py
@@ -4,7 +4,6 @@ import os
 from mavenpackage import return_event
 
 
-
 '''
 The following tests are applied to the return_event function.
 First, valid entries are tested: the function receives expected inputs.
@@ -12,27 +11,27 @@ Next, the function is given invalid entries and its reaction is checked.
 Then, the performance when no inputs are provided is evaluated.
 '''
 
+
 class TestInput(unittest.TestCase):
 
-    # smoke test: valid inputs
+    # Smoke test: valid inputs
     def test_correct_values(self):
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(return_event.return_event("2023-12-26"), "St. Stephen's Day")
+        # You should select some valid inputs, for which the output is known
+        self.assertEqual(return_event.return_event("2023-12-26"), "St. \
+        Stephen's Day")
 
-    # invalid inputs
+    # Invalid inputs
     def test_wrong_values(self):
-        # you should input wrong data
+        # You should input wrong data
         self.assertEqual(return_event.return_event("St. Stephen's Day"), None)
 
         # Note: the following test passing an empty list will fail!
         # self.assertEqual(return_event.return_event([]), None)
 
-    # corner case: empty string
+    # Corner case: empty string
     def test_empty_string(self):
         self.assertEqual(return_event.return_event(""), None)
 
-# code that runs the document if called
-if __name__ == '__main__':
+# Code that runs the document if called
+if __name__ == "__main__":
     unittest.main()
-
-
