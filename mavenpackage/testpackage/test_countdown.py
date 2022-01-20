29b4c7aec932dbf3474c9f86bdd7285402e62b65
Martina Turus <martina210300@gmail.com>
2022-01-20 18:19:20 +0100

    Fixed error and Final PEP8 check

--- mavenpackage/testpackage/test_countdown.py
+++ mavenpackage/testpackage/test_countdown.py
@@ -15,17 +15,17 @@ class TestInput(unittest.TestCase):
     # Invalid inputs
     def test_wrong_values(self):
         # You should input wrong data
-        self.assertEqual(countdown.countdown("2022-01-32"), "Incorrect data \
-        format, should be YYYY-MM-DD")
-        self.assertEqual(countdown.countdown("hello"), "Incorrect data \
-        format, should be YYYY-MM-DD")
-        self.assertEqual(countdown.countdown("67"), "Incorrect data format, \
-        should be YYYY-MM-DD")
+        self.assertEqual(countdown.countdown("2022-01-32"), "Incorrect data " +
+        "format, should be YYYY-MM-DD")
+        self.assertEqual(countdown.countdown("hello"), "Incorrect data " +
+        "format, should be YYYY-MM-DD")
+        self.assertEqual(countdown.countdown("67"), "Incorrect data " +
+        "format, should be YYYY-MM-DD")
 
         # Corner case: empty string
     def test_empty_string(self):
-        self.assertEqual(countdown.countdown(""), "Incorrect data format, \
-        should be YYYY-MM-DD")
+        self.assertEqual(countdown.countdown(""), "Incorrect data " +
+        "format, should be YYYY-MM-DD")
 
 
 # Code that runs the document if called
