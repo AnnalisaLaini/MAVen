954902181f7be69e7e9e1cb8ffff4c8ade81f525
vanepo <vanessa.pozzi@student.h-farm.com>
2022-01-20 17:50:43 +0100

    solve errors

--- mavenpackage/testpackage/test_leapyear.py
+++ mavenpackage/testpackage/test_leapyear.py
@@ -15,18 +15,17 @@ class TestInput(unittest.TestCase):
     # Smoke test: valid inputs
     def test_leapyear(self):
         # You should select some valid inputs, for which the output is known
-        self.assertEqual(leapyear.leapyear("2000"), "'{0} is a leap \
-        year'.format(year)")
+        self.assertEqual(leapyear.leapyear("2000"), "2000 is a leap year")
 
     # Wrong inputs
     def test_not_leapyear(self):
         # You should input wrong data
-        self.assertEqual(leapyear.leapyear("hello"), None)
-        self.assertEqual(leapyear.leapyear("twothousand"), None)
+        self.assertEqual(leapyear.leapyear("hello"), "Input date is not valid")
+        self.assertEqual(leapyear.leapyear("twothousand"), "Input date is not valid")
 
     # Corner case: empty string
     def test_emptyentry(self):
-        self.assertEqual(leapyear.leapyear(""), None)
+        self.assertEqual(leapyear.leapyear(""), "Input date is not valid")
 
 
 # Code that runs the document if called
