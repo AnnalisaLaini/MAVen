8366ac5cb21765d7235988b17335decbcbae1c79
Martina Turus <martina210300@gmail.com>
2022-01-20 14:26:12 +0100

    Final PEP8 check

--- mavenpackage/testpackage/test_italianceleb.py
+++ mavenpackage/testpackage/test_italianceleb.py
@@ -32,6 +32,7 @@ class TestInput(unittest.TestCase):
     def test_emptyentry(self):
         self.assertEqual(italianceleb.italianceleb(""), None)
 
+
 # Code that runs the document if called
 if __name__ == "__main__":
     unittest.main()
