0e338f3d4c02969378ebeed2a9376b8485917e57
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-02-13 05:45:29 -0500

    Final PEP8 Check

--- mavenpackage/testpackage/test_return_event.py
+++ mavenpackage/testpackage/test_return_event.py
@@ -22,8 +22,8 @@ class TestInput(unittest.TestCase):
     def test_wrong_values(self):
         # You should input wrong data
         self.assertEqual(return_event.return_event("St. Stephen's Day"),
-                         "Sadly, we don't know what is\
-                          celebrated on St. Stephen's Day")
+                         "Sadly, we don't know what is" +
+                         "celebrated on St. Stephen's Day")
 
         # Note: the following test passing an empty list will fail!
         # self.assertEqual(return_event.return_event([]), None)
@@ -31,8 +31,8 @@ class TestInput(unittest.TestCase):
     # Corner case: empty string
     def test_empty_string(self):
         self.assertEqual(return_event.return_event(""),
-                         "Sadly, we don't know what is \
-                         celebrated on ")
+                         "Sadly, we don't know what is" +
+                         " celebrated on ")
 
 
 # Code that runs the document if called
