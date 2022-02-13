aa12ea6fb504ab4a1efedbadec4047488b609d8a
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2022-02-13 05:11:13 -0500

    Final PEP8 Check

--- mavenpackage/testpackage/test_italianceleb.py
+++ mavenpackage/testpackage/test_italianceleb.py
@@ -14,25 +14,26 @@ class TestInput(unittest.TestCase):
 
     # Smoke test: valid inputs
     def test_italianholidays(self):
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(italianceleb.italianceleb("All Saints' Day"), "Yup,"+
-       " All Saints' Day is an Italian holiday!")
+        # You should select some valid inputs, for which the output is known
+        self.assertEqual(italianceleb.italianceleb("All Saints' Day"),
+                         "Yup," + " All Saints' Day is an Italian holiday!")
 
     def test_nonitalian(self):
-       # You should input wrong data
-       self.assertEqual(italianceleb.italianceleb("Thanksgiving Day"), "Nope"+
-       ", Italians do not celebrate Thanksgiving Day.")
+        # You should input wrong data
+        self.assertEqual(italianceleb.italianceleb("Thanksgiving Day"),
+                         "Nope" + ", Italians do not celebrate\
+                         Thanksgiving Day.")
 
     # Invalid inputs
     def test_wrongvalues(self):
-       # You should input wrong data
-       self.assertEqual(italianceleb.italianceleb("78"), "Nope"+
-       ", Italians do not celebrate 78.")
+        # You should input wrong data
+        self.assertEqual(italianceleb.italianceleb("78"), "Nope" +
+                         ", Italians do not celebrate 78.")
 
     # Corner case: empty string
     def test_emptyentry(self):
-        self.assertEqual(italianceleb.italianceleb(""), "Nope"+
-       ", Italians do not celebrate .")
+        self.assertEqual(italianceleb.italianceleb(""), "Nope" +
+                         ", Italians do not celebrate .")
 
 
 # Code that runs the document if called
